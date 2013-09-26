d318bf71535f429d602997e88db32eebaf7f4b84
Thomas Lynge <tlynge@gmail.com>
2013-09-26 22:01:32 +0200

    Search pagination.

--- src/public_html/app/mods/search.js
+++ src/public_html/app/mods/search.js
@@ -1,54 +1,95 @@
 // Search Module
-define(['knockout', 'factory/object' ], 
-function (ko, objfac) {
+define(['knockout', 'factory/object'],
+        function(ko, objfac) {
 
-    var pagesize = ko.observable(20);
-    var pageindex = ko.observable(0);
-    var freetext = ko.observable("");
-    var items = ko.observableArray([]);
-    var pagingitems = ko.observableArray([]);
-    
-    function searchReceived(response)
-    {    
-        console.log(response);
-        items.removeAll();
-        for(var i = 0; i < response.Body.Count; i++)
-        {
-            var r = response.Body.Results[i];
-            var oi = new objfac.ObjectItem();
-            oi.title = "Title : " + r.Title;
-            if(r.Type == "Radio")
-                oi.hash = '#!object/id='+r.Id;
-            else
-                oi.hash = r.Url;
-            items.push(oi);
-        }
-        
-        pagingitems.removeAll();
-        
-        var backcls = "";
-        if(pageindex()==0)
-            backcls = "disabled";
-        
-        pagingitems.push({text:"&laquo;", index:0, cssclass:backcls});
-        pagingitems.push({text:"1", index:1, cssclass: "active"});
-        pagingitems.push({text:"2", index:2, cssclass: ""});
-        pagingitems.push({text:"3", index:3, cssclass: ""});
-        pagingitems.push({text:"4", index:4, cssclass: ""});
-        pagingitems.push({text:"&raquo;", index:5, cssclass: ""});
-    }
-    
-    return {
-        items: items,
-        pagingitems: pagingitems,
-        pageindex: pageindex,
-        pagesize: pagesize,
-        freetext: freetext,
-        
-        search: function(){
-            CHAOS.Portal.Client.View.Get(Settings.Search.viewName,freetext(),"","",pageindex(),pagesize()).WithCallback(searchReceived);
-        }
-    };
-});
+            // Paging
+            var pagesize = ko.observable(20);
+            var pageindex = ko.observable(0);
+            var pagingitems = ko.observableArray([]);   // UI paging items. Should probably be moved from here.
+            var pageindexshown = ko.observable(4);
+            var totalcount = 0;
+            var noofpages = 0;
+            var freetext = ko.observable("");
+            var items = ko.observableArray([]);
+
+            function searchReceived(response)
+            {
+                console.log(response);
+                items.removeAll();
+                for (var i = 0; i < response.Body.Count; i++)
+                {
+                    var r = response.Body.Results[i];
+                    var oi = new objfac.ObjectItem();
+                    oi.title = "Title : " + r.Title;
+                    if (r.Type == "Radio")
+                        oi.hash = '#!object/id=' + r.Id;
+                    else
+                        oi.hash = r.Url;
+                    items.push(oi);
+                }
+
+                totalcount = response.Body.TotalCount;
+                noofpages = Math.ceil(totalcount / pagesize());
+
+                updatePaging();
+            }
+
+            function updatePaging() {
+                pagingitems.removeAll();
+
+//var backcls = "";
+//if(pageindex()==0)
+//  backcls = "disabled";        
+//pagingitems.push({text:"&laquo;", index:-1, cssclass:backcls});
+
+                if (pageindex() >= pageindexshown())
+                    pagingitems.push({text: "&laquo;", index: -1, cssclass: ""});
+
+                var pirange = pageindexrange(pageindex());
+
+                for (var i = pirange[0]; i < pirange[1]; i++) {
+                    pagingitems.push({text: "" + (i + 1), index: i, cssclass: (i == pageindex() ? "active" : "")});
+                }
+
+                var nextpagegroup =   pageindexrange(pageindex() + pageindexshown() + 1);
+                if(nextpagegroup[0] < nextpagegroup[1])
+                    pagingitems.push({text: "&raquo;", index: 9999, cssclass: ""});
+
+            }
+
+            function nextPageGroup() {
+                pageindex(pageindexrange(pageindex() + pageindexshown() + 1)[0]);
+            }
+
+            function prevPageGroup() {
+                pageindex(pageindexrange(pageindex() - pageindexshown())[0]);
+            }
+
+            function pageindexrange(index) {
+                var b = Math.floor(Math.max(0, index) / pageindexshown())*pageindexshown();
+                var e = b + pageindexshown();
+                if (e > noofpages)
+                    e = noofpages;
+                return[b, e];
+            }
+
+            return {
+                items: items,
+                pagingitems: pagingitems,
+                pageindex: pageindex,
+                pagesize: pagesize,
+                pageindexshown: pageindexshown,
+                freetext: freetext,
+                nextPageGroup: nextPageGroup,
+                prevPageGroup: prevPageGroup,
+                search: function() {
+
+                    if (pageindex() < 0)
+                        pageindex(0);
+
+                    CHAOS.Portal.Client.View.Get(Settings.Search.viewName, freetext(), "", "", pageindex(), pagesize()).WithCallback(searchReceived);
+                }
+            };
+        });
 
 
--- src/public_html/app/viewmodels/search.js
+++ src/public_html/app/viewmodels/search.js
@@ -1,32 +1,41 @@
 // Search View
-define(['durandal/app', 'knockout', 'mods/search', 'mods/state'], 
-function (app, ko, search, state) {
-        
-    var name = ko.observable();
-        
-    return {
-        items: search.items,
-        pagingitems: search.pagingitems,
-        name: name,
-        userEmail: "thfl@dr.dk",
-        title: "Search Panel",
-        activate: function (param) {
-        
-            if (param != undefined)
-            {
-                var s = state.getParamByName('s', param);
-                search.freetext(s);
-                search.search();
-            }       
-        },
-         // http://durandaljs.com/documentation/Hooking-Lifecycle-Callbacks/       
-        compositionComplete: function () {
-            $("#SearchField").focus();
-        },
-        pagingclick: function(data){
-            console.log("----------");
-            console.log(data);
-            console.log("----------");
-        }        
-    };
-});
+define(['durandal/app', 'knockout', 'mods/search', 'mods/state'],
+        function(app, ko, search, state) {
+
+            var name = ko.observable();
+
+            return {
+                items: search.items,
+                pagingitems: search.pagingitems,
+                name: name,
+                userEmail: "thfl@dr.dk",
+                title: "Search Panel",
+                activate: function(param) {
+
+                    if (param != undefined)
+                    {
+                        var s = state.getParamByName('s', param);
+                        search.pageindex(0);
+                        search.freetext(s);
+                        search.search();
+                    }
+                },
+                // http://durandaljs.com/documentation/Hooking-Lifecycle-Callbacks/       
+                compositionComplete: function() {
+                    $("#SearchField").focus();
+                },
+                pagingclick: function(data) {
+
+                    if (data.index == -1)
+                        search.prevPageGroup();
+                    else if (data.index == 9999)
+                        search.nextPageGroup();
+                    else
+                        search.pageindex(data.index);
+
+                    search.search();
+                }
+
+
+            };
+        });
