4b6d5aa7c77f2fc8fd353bd204809f42d61d6c1a
Thomas Lynge <tlynge@gmail.com>
2013-10-03 14:26:54 +0200

    Calendar year, months and days.

--- src/public_html/app/mods/format.js
+++ src/public_html/app/mods/format.js
@@ -1,8 +1,33 @@
 define(function() {
 
-    function getSolrDateStr(date) {        
+    if (!Date.prototype.toISOString) {
+        (function() {
+
+            function pad(number) {
+                var r = String(number);
+                if (r.length === 1) {
+                    r = '0' + r;
+                }
+                return r;
+            }
+
+            Date.prototype.toISOString = function() {
+                return this.getUTCFullYear()
+                        + '-' + pad(this.getUTCMonth() + 1)
+                        + '-' + pad(this.getUTCDate())
+                        + 'T' + pad(this.getUTCHours())
+                        + ':' + pad(this.getUTCMinutes())
+                        + ':' + pad(this.getUTCSeconds())
+                        + '.' + String((this.getUTCMilliseconds() / 1000).toFixed(3)).slice(2, 5)
+                        + 'Z';
+            };
+
+        }());
+    }
+
+    function getSolrDateStr(date) {
         var ds = date.getFullYear() + "-" +
-                getDigit2(date.getMonth()+1) + "-" +
+                getDigit2(date.getMonth() + 1) + "-" +
                 getDigit2(date.getDate()) + "T" +
                 getDigit2(date.getHours()) + ":" +
                 getDigit2(date.getMinutes()) + ":" +
@@ -12,8 +37,8 @@ define(function() {
         return ds;
     }
 
-    function getQueryDateStr(date){
-        return date.getFullYear() + '-' + getDigit2(date.getMonth()+1) + '-' + getDigit2(date.getDate());
+    function getQueryDateStr(date) {
+        return date.getFullYear() + '-' + getDigit2(date.getMonth() + 1) + '-' + getDigit2(date.getDate());
     }
 
     function getDigit2(num) {
@@ -24,15 +49,16 @@ define(function() {
         return ("00" + num).slice(-3);
     }
 
-    function getDateFromQueryDateStr(yyyymmdd){
+    function getDateFromQueryDateStr(yyyymmdd) {
         var d = Date.parse(yyyymmdd);
-        if(isNaN(d))
+        if (isNaN(d))
             return null;
-   
-        return new Date(d);
+
+        var date = new Date(d);
+        return date;
     }
 
-    function getSolrFilterFromDateRangeStr(solrfield, yyyymmdd1,yyyymmdd2){
+    function getSolrFilterFromDateRangeStr(solrfield, yyyymmdd1, yyyymmdd2) {
         var d1 = getDateFromQueryDateStr(yyyymmdd1);
         var d2 = getDateFromQueryDateStr(yyyymmdd2);
         var ds1 = getSolrDateStr(d1);
--- src/public_html/app/mods/searchcalendar.js
+++ src/public_html/app/mods/searchcalendar.js
@@ -10,11 +10,36 @@ define(['knockout', 'factory/calendar', 'mods/format'], function(ko, calfac, for
         filterwithoutdates = pfilterwithoutdates;
         search = searchmethod;
 
-        if (pdatebegin == null || pdateend == null) {
-            //Decades
-            dodecades();
+        var any = false;
+
+        if (pdatebegin != null && pdateend != null) {
+
+            var d1 = pdatebegin;
+            var d2 = pdateend;
+            var d1yyyy = "" + d1.getFullYear();
+            var d2yyyy = "" + d2.getFullYear();
+
+            if (
+                    d1yyyy.substring(0, 3) == d2yyyy.substring(0, 3) &&
+                    d1yyyy.slice(-1) == "0" && d2yyyy.slice(-1) == "9"
+                    ) {
+
+                any = true;
+                doyears(d1.getFullYear());
+            }
+            else if (d1yyyy == d2yyyy && d1.getMonth() != d2.getMonth()) {
+                any = true;
+                domonths(d1.getFullYear());
+            }
+            else if (d1yyyy == d2yyyy && d1.getMonth() == d2.getMonth()) {
+                any = true;
+                dodays(d1.getFullYear(), d1.getMonth());
+            }
         }
 
+        if (!any) {
+            dodecades();
+        }
     }
 
     function dodecades() {
@@ -28,7 +53,56 @@ define(['knockout', 'factory/calendar', 'mods/format'], function(ko, calfac, for
             item.search = search;
             item.title(i + "'erne");
             item.datebegin(i + "-01-01");
+            item.dateend(i + 9 + "-12-31");
+            items.push(item);
+            item.load(freetext, filterwithoutdates);
+        }
+    }
+
+    function doyears(year) {
+        items.removeAll();
+
+        var yearb = year;
+        var yeare = year + 9;
+
+        for (var i = yearb; i < yeare; i++) {
+            var item = new calfac.CalendarItem();
+            item.search = search;
+            item.title(i);
+            item.datebegin(i + "-01-01");
             item.dateend(i + "-12-31");
+            items.push(item);
+            item.load(freetext, filterwithoutdates);
+        }
+    }
+
+    function domonths(y) {
+
+        items.removeAll();
+
+        for (var m = 1; m < 13; m++) {
+            var item = new calfac.CalendarItem();
+            item.search = search;
+            item.title(format.getDigit2(m) + "-" + y);
+            item.datebegin(y + "-" + format.getDigit2(m) + "-01");
+            var lastDay = new Date(y, m, 0);
+            item.dateend(y + "-" + format.getDigit2(m) + "-" + format.getDigit2(lastDay.getDate()));
+            items.push(item);
+            item.load(freetext, filterwithoutdates);
+        }
+    }
+
+    function dodays(y,m) {
+
+        items.removeAll();
+        var lastDay = new Date(y, m+1, 0);
+        
+        for (var d = 1; d < lastDay.getDate(); d++) {
+            var item = new calfac.CalendarItem();
+            item.search = search;
+            item.title(format.getDigit2(d) + "-" + format.getDigit2(m+1) + "-" + y);
+            item.datebegin(y + "-" + format.getDigit2(m+1) + "-" + format.getDigit2(d));
+            item.dateend(y + "-" + format.getDigit2(m+1) + "-" + format.getDigit2(d));
             items.push(item);
             item.load(freetext, filterwithoutdates);
         }
