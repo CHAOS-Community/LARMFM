aede3b41fa6471b7efb582db2e078bfe708bc987
Thomas Lynge <tlynge@gmail.com>
2013-11-06 16:24:40 +0100

    Textarea

--- src/public_html/app/mods/jsonformfields.js
+++ src/public_html/app/mods/jsonformfields.js
@@ -14,6 +14,33 @@
     };
 
     JSONForm.fieldTypes['wsstring'] = {
+        'template': '<textarea id="<%= id %>" name="<%= node.name %>" ' +
+          'style="height:<%= elt.height || "28px" %>;width:<%= elt.width || "100%" %>;"' +
+          '<%= (node.disabled? " disabled" : "")%>' +
+          '<%= (node.readOnly ? " readonly=\'readonly\'" : "") %>' +
+          '<%= (node.schemaElement && node.schemaElement.maxLength ? " maxlength=\'" + node.schemaElement.maxLength + "\'" : "") %>' +
+          '<%= (node.schemaElement && node.schemaElement.required ? " required=\'required\'" : "") %>' +
+          '<%= (node.placeholder? "placeholder=" + \'"\' + escape(node.placeholder) + \'"\' : "")%>' +
+          '><%= value %></textarea>',
+        'fieldtemplate': true,
+        'inputfield': true,
+        onInsert: function (evt, node) {
+            var selector = "#" + node.id.replace(/\./g, "\\.");
+
+            var $element = $(selector).get(0);
+            if ($element !== undefined) {
+                $element.addEventListener('keyup', function () {
+                    this.style.overflow = 'hidden';
+                    var hgt = this.clientHeight;
+                    if (hgt < this.scrollHeight) {
+                        this.style.height = (this.scrollHeight + 32) + 'px';
+                    }
+                }, false);
+            }
+        }
+    };
+
+    JSONForm.fieldTypes['wsstring2'] = {
         template: '<textarea style="width:90%;height:28px;" ' +
   '<%= (fieldHtmlClass ? "class=\'" + fieldHtmlClass + "\' " : "") %>' +
   'name="<%= node.name %>" value="<%= escape(value) %>" id="<%= id %>"' +
