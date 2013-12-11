275d93594ed6d8afe509939fa98c9bab578ed683
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-12-11 13:17:42 +0100

    Added some cross browser handling of wayf login.

--- src/public_html/app/viewmodels/wayflogin.js
+++ src/public_html/app/viewmodels/wayflogin.js
@@ -1,14 +1,29 @@
 ﻿define(['plugins/dialog', 'mods/portal', 'jquery'], function (dialog, portal, jquery) {
 	var wayflogin = function () {
-		
+		this._wayfWindow = null;
+		this.CanUseIframe = window && window.navigator && window.navigator.userAgent && (window.navigator.userAgent.indexOf("Chrome") != -1 || window.navigator.userAgent.indexOf("Firefox") != -1) && false;
 	};
 
 	wayflogin.prototype.compositionComplete = function (child, parent, settings) {
+		if (!this.CanUseIframe) return;
+
+		var frame = jquery(child).find(".LoginFrame")[0];
+
 		var that = this;
 		CHAOS.Portal.Client.Wayf.Login(Settings.wayfPath, jquery(child).find(".LoginFrame")[0], function (success) { that.LoginCompleted(success); });
 	};
 
-	wayflogin.prototype.LoginCompleted = function(success) {
+	wayflogin.prototype.OpenWindow = function() {
+		var that = this;
+		this._wayfWindow = window.open("", "WayfLogin", "width=800,height=800,menubar=no,status=no,toolbar=no,resizable=yes");
+
+		CHAOS.Portal.Client.Wayf.Login(Settings.wayfPath, this._wayfWindow, function(success) { that.LoginCompleted(success); });
+	};
+
+	wayflogin.prototype.LoginCompleted = function (success) {
+		if (this._wayfWindow != null)
+			this._wayfWindow.close();
+		
 		dialog.close(this);
 	};
 
