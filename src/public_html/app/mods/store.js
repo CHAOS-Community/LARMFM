e4d3b996ebdf0f29fd1e7b603c68b464b026bb17
Thomas Lynge <tlynge@gmail.com>
2013-12-13 09:23:42 +0100

    SessionGuid in cookie.

---
+++ src/public_html/app/mods/store.js
@@ -0,0 +1,40 @@
+﻿define(function () {
+
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+    function cookie(name, value, days) {
+        if (value)
+            return createCookie(name, value, days);
+
+        return readCookie(name);
+    }
+
+    return {
+        cookie: cookie,
+        cookieDelete: eraseCookie
+    };
+
+});
