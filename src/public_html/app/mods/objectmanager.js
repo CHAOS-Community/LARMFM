bc77447645f77780285ef7726aa5ca163c0114e7
Thomas Lynge <tlynge@gmail.com>
2014-01-20 12:40:54 +0100

    Refactored objectmanager.

--- src/public_html/app/mods/objectmanager.js
+++ src/public_html/app/mods/objectmanager.js
@@ -4,26 +4,47 @@
 
     function metadataReceived(data) {
         var r = data.Body.Results[0];
+        doCallback(r.Id, r);
+    }
 
-        var cb = callbacks[r.Id];
-        if ( cb !== undefined) {
+    function doCallback(guid, param) {
+        var cb = callbacks[guid];
+        if (cb !== undefined) {
             while (cb.length > 0) {
-                cb.shift()(r);
+                cb.shift()(param);
             }
 
-            if(cb.length == 0)
-                delete callbacks[r.Id];
+            if (cb.length == 0)
+                delete callbacks[guid];
         }
     }
 
+    function generateGUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+        });
+        return uuid;
+    }
+
+    function pushCallback(guid, callback) {
+        if (callbacks[guid] === undefined) {
+            callbacks[guid] = [];
+        }
+
+        callbacks[guid].push(callback);
+    }
+
+    // --- Public properties and methods ---
     return {
+        //-------------------------------------------------
+        generateGUID: generateGUID,
+        //-------------------------------------------------
         getByGuid: function (guid, callback) {
 
-            if (callbacks[guid] === undefined) {
-                callbacks[guid] = [];
-            }
-
-            callbacks[guid].push(callback);
+            pushCallback(guid, callback);
 
             // Object.Get
             // ---------------------------------------------------------
@@ -38,6 +59,13 @@
                 true, true, true,
                 false, false, 1, 0,
                 null).WithCallback(metadataReceived);
+        },
+        //-------------------------------------------------
+        createObject: function (callback) {
+            var guid = generateGUID();
+            pushGuid(guid, callback);
+
         }
+        //-------------------------------------------------
     };
 });
