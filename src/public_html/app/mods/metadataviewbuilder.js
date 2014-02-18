39025947b7cf09a07f7fd36a5166d670023c72ab
Thomas Lynge <tlynge@gmail.com>
2014-02-18 13:40:30 +0100

    Metadata view builder.

--- src/public_html/app/mods/metadataviewbuilder.js
+++ src/public_html/app/mods/metadataviewbuilder.js
@@ -11,62 +11,104 @@
         return t;
     }
 
+    function txt(t) {
+        if (t)
+            return t;
+
+        return "";
+    }
+
     function titlehtml(title) {
         return '<h5 class="dotted">' + loc(title) + '</h5>';
     }
 
+    function getArray(arr) {
+
+        if (Array.isArray(arr))
+            return arr;
+
+        for (var prop in arr) {
+            if (arr.hasOwnProperty(prop) && Array.isArray(arr[prop]))
+                return arr[prop];
+        }
+
+        return null;
+    }
+
     return {
-        mdheadline: function(parent, title) {
+        mdheadline: function (parent, title) {
             parent.push(this.headline(title));
         },
         headline: function (title) {
             return '<h4>' + loc(title) + '</h4>';
         },
-        mdtext: function(parent, title, text) {
+        mdtext: function (parent, title, text) {
             parent.push(this.text(title, text));
         },
         text: function (title, text) {
-            return  titlehtml(title) + '<div class="md-text">' + text + '</div>';
+            return titlehtml(title) + '<div class="md-text">' + text + '</div>';
         },
-        mdtags: function(parent, title, tagarray) {
-            parent.push(this.tags(title, tagarray));
+        mdtags: function (parent, title, tagarray, prop) {
+            parent.push(this.tags(title, tagarray, prop));
         },
-        tags: function (title, tagarray) {
+        tags: function (title, tagarray, prop) {
             var t = titlehtml(title) + '<div class="md-tags">';
 
             var arr = tagarray;
             if (typeof arr === "string") {
                 //OpenEnumerationList_Tags
                 arr = arr.split(" ");
+            } else {
+
+                if (prop) {
+                    if (arr.hasOwnProperty(prop)) {
+                        arr = getArray(arr[prop]);
+                    } else {
+                        arr = null;
+                    }
+
+                } else
+                    arr = getArray(arr);
             }
 
-            for (var i = 0; i < arr.length; i++) {
-                t += '<button class="btn btn-tag" style="margin-left: 4px;">' + arr[i] + '</button>'
+            if (arr != null) {
+                for (var i = 0; i < arr.length; i++) {
+                    if (arr[i] !== "")
+                        t += '<button class="btn btn-tag" style="margin-left: 4px;">' + arr[i] + '</button>'
+                }
             }
+
             t += '</div>';
             return t;
         },
-        mdtable: function(parent, title, dataarray, titlearray, fieldarray) {
+        mdtable: function (parent, title, dataarray, titlearray, fieldarray) {
             parent.push(this.table(title, dataarray, titlearray, fieldarray));
         },
         table: function (title, dataarray, titlearray, fieldarray) {
-            var t = titlehtml(title) + '<table class="md-table table table-bordered">';
-            t += '<tr>';
-            for (var i = 0; i < titlearray.length; i++) {
-                t += '<th>' + loc(titlearray[i]) + '</th>';
-            }
-            t += '</tr>';
-            for (var i = 0; i < dataarray.length; i++) {
+
+            var da = getArray(dataarray);
+
+            var t = titlehtml(title);
+            if (da != null && da.length > 0 && typeof da[0] !== "string") {
+
+                t += '<table class="md-table table table-bordered">';
                 t += '<tr>';
-                for (var j = 0; j < fieldarray.length; j++) {
-                    t += '<td>' + dataarray[i][fieldarray[j]] + '</td>';
+                for (var i = 0; i < titlearray.length; i++) {
+                    t += '<th>' + loc(titlearray[i]) + '</th>';
                 }
                 t += '</tr>';
+                for (var i = 0; i < da.length; i++) {
+                    t += '<tr>';
+                    for (var j = 0; j < fieldarray.length; j++) {
+                        t += '<td>' + da[i][fieldarray[j]] + '</td>';
+                    }
+                    t += '</tr>';
+                }
+                t += '</table>';
             }
-            t += '</table>';
             return t;
         },
-        mdgrid: function(parent, title, gridarray) {
+        mdgrid: function (parent, title, gridarray) {
             parent.push(this.grid(title, gridarray));
         },
         grid: function (title, gridarray) {
@@ -75,7 +117,7 @@
             for (var i = 0; i < gridarray.length; i++) {
                 t += '<tr>';
                 for (var j = 0; j < gridarray[i].length; j++) {
-                    t += '<td>' + gridarray[i][j] + '</td>';
+                    t += '<td>' + txt(gridarray[i][j]) + '</td>';
                 }
                 t += '</tr>';
             }
--- src/public_html/app/views/metadata/larmprogram.js
+++ src/public_html/app/views/metadata/larmprogram.js
@@ -46,21 +46,21 @@
                 if (metadata[i].MetadataSchemaGuid == larm_metadata_guid) {
 
                     // --- TEST DATA
-                    metadata[i].MetadataXml =
-                        "<Larm.Metadata>" +
-                        "<Title>Dramatisk forum - Poul Reumert</Title>" +
-                        "<Description>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.</Description>" +
-                        "<Genre>svendborg politi kontor banegård</Genre>" +
-                        "<Subjects>svendborg politi kontor banegård</Subjects>" +
-                        "<Tags>svendborg politi kontor banegård</Tags>" +
-                        "<Note>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.</Note>" +
-                        "<RelatedObjects>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.</RelatedObjects>" +
-                        "<Contributors>" +
-                            "<Contributor><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
-                            "<Contributor><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
-                            "<Contributor><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
-                        "</Contributors>" +
-                        "</Larm.Metadata>";
+                    //metadata[i].MetadataXml =
+                    //    "<Larm.Metadata>" +
+                    //    "<Title>Dramatisk forum - Poul Reumert</Title>" +
+                    //    "<Description>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.</Description>" +
+                    //    "<Genre>svendborg politi kontor banegård</Genre>" +
+                    //    "<Subjects>svendborg politi kontor banegård</Subjects>" +
+                    //    "<Tags>svendborg politi kontor banegård</Tags>" +
+                    //    "<Note>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.</Note>" +
+                    //    "<RelatedObjects>Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.</RelatedObjects>" +
+                    //    "<Contributors>" +
+                    //        "<Contributor><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
+                    //        "<Contributor><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
+                    //        "<Contributor><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><Subject>Nullam varius, turpis et commodo pharetra.</Subject></Contributor>" +
+                    //    "</Contributors>" +
+                    //    "</Larm.Metadata>";
 
                     self.m0 = handlexml(metadata[i]);
                     larm = self.m0.Larm_Metadata;
@@ -70,29 +70,28 @@
                 else if (metadata[i].MetadataSchemaGuid == larm_program_guid) {
 
                     // --- TEST DATA
-                    metadata[i].MetadataXml =
-                        "<Larm.Program>" +
-                        "<PublicationDateTime>0101-01-01T00:00:00</PublicationDateTime><PublicationEndDateTime>0101-01-01T00:08:50</PublicationEndDateTime>" +
-                        "<PublicationChannel>DR P1</PublicationChannel>" +
-                        "<Title>Tysk propaganda-udsendelse - Reportage fra østfronten</Title>" +
-                        "<Abstract>Søren Aabye Kierkegaard (5. maj 1813 – 11. november 1855) var en dansk teolog og filosof. Kierkegaard regnes ofte som den største danske filosof og som fader til eksistentialismen. Efter Jean-Paul Sartre og den ateistiske eksistentialisme benævnes Kierkegaard dog ofte som hovedskikkelse i den kristne eksistentialisme.</Abstract>" +
-                        "<Description>Det er karakteristisk, at værkerne fra forfatterskabets første fase er udgivet under pseudonymer. I Efterskriften vedkender Kierkegaard sig samtlige sine værker. Ved hjælp af pseudonymerne lader han de teologiske, filosofiske og psykologiske temaer spille dialektisk mod hinanden på en måde, som har gjort ham til en af eksistentialismens fædre. Pseudonymerne har netop gjort, at forståelsen af hans værker blev op til 'hiin Enkelte', fordi argumentationen 'ikke' var fremført af Kierkegaard selv. Han udgav samtidig en række religiøse 'taler', som blev samlet i 1845 under titlen Atten opbyggelige Taler. Han udgav dem under eget navn for at vise, at han først og fremmest opfattede sig selv som religiøs og kristelig forfatter. I den sidste periode – kirkestormen – kaldte han hele sit tidligere forfatterskab for 'en digters politieklogskab' der skulle gøre de mennesker, han nu angreb, nemlig præsterne, trygge, sådan som politiet gør det for at se dybere i tingene.[1] Han betragtes også som en af den danske guldalders hovedpersoner.</Description>" +
-                        "<Publisher>DR</Publisher>" +
-                        "<Subjects><Subject>Radio</Subject><Subject>Dronning Margrethe 2.</Subject></Subjects>" +
-                        "<Contributors>" +
-                            "<Contributor><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><RoleID>1</RoleID></Contributor>" +
-                            "<Contributor><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><RoleID>2</RoleID></Contributor>" +
-                            "<Contributor><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><RoleID>3</RoleID></Contributor>" +
-                        "</Contributors>" +
-                        "<Creators>" +
-                            "<Creator><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><RoleID>1</RoleID></Creator>" +
-                            "<Creator><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><RoleID>2</RoleID></Creator>" +
-                            "<Creator><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><RoleID>3</RoleID></Creator>" +
-                        "</Creators>" +
-                        "<Locations><Name>Svendborg</Name><Name>Svendborg havn</Name><Name>Svendborg banegård</Name></Locations>" +
-                        "<Identifiers><DR.ProductionNumber>666-666-6666</DR.ProductionNumber><DR.ArchiveNumber>2023</DR.ArchiveNumber><SB.DomsID>99</SB.DomsID></Identifiers></Larm.Program>";
-                    //"<Contributor><Name></Name><RoleName></RoleName><RoleID></RoleID></Contributor>" +
-                    // ---
+                    //metadata[i].MetadataXml =
+                    //    "<Larm.Program>" +
+                    //    "<PublicationDateTime>0101-01-01T00:00:00</PublicationDateTime><PublicationEndDateTime>0101-01-01T00:08:50</PublicationEndDateTime>" +
+                    //    "<PublicationChannel>DR P1</PublicationChannel>" +
+                    //    "<Title>Tysk propaganda-udsendelse - Reportage fra østfronten</Title>" +
+                    //    "<Abstract>Søren Aabye Kierkegaard (5. maj 1813 – 11. november 1855) var en dansk teolog og filosof. Kierkegaard regnes ofte som den største danske filosof og som fader til eksistentialismen. Efter Jean-Paul Sartre og den ateistiske eksistentialisme benævnes Kierkegaard dog ofte som hovedskikkelse i den kristne eksistentialisme.</Abstract>" +
+                    //    "<Description>Det er karakteristisk, at værkerne fra forfatterskabets første fase er udgivet under pseudonymer. I Efterskriften vedkender Kierkegaard sig samtlige sine værker. Ved hjælp af pseudonymerne lader han de teologiske, filosofiske og psykologiske temaer spille dialektisk mod hinanden på en måde, som har gjort ham til en af eksistentialismens fædre. Pseudonymerne har netop gjort, at forståelsen af hans værker blev op til 'hiin Enkelte', fordi argumentationen 'ikke' var fremført af Kierkegaard selv. Han udgav samtidig en række religiøse 'taler', som blev samlet i 1845 under titlen Atten opbyggelige Taler. Han udgav dem under eget navn for at vise, at han først og fremmest opfattede sig selv som religiøs og kristelig forfatter. I den sidste periode – kirkestormen – kaldte han hele sit tidligere forfatterskab for 'en digters politieklogskab' der skulle gøre de mennesker, han nu angreb, nemlig præsterne, trygge, sådan som politiet gør det for at se dybere i tingene.[1] Han betragtes også som en af den danske guldalders hovedpersoner.</Description>" +
+                    //    "<Publisher>DR</Publisher>" +
+                    //    "<Subjects><Subject>Radio</Subject><Subject>Dronning Margrethe 2.</Subject></Subjects>" +
+                    //    "<Contributors>" +
+                    //        "<Contributor><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><RoleID>1</RoleID></Contributor>" +
+                    //        "<Contributor><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><RoleID>2</RoleID></Contributor>" +
+                    //        "<Contributor><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><RoleID>3</RoleID></Contributor>" +
+                    //    "</Contributors>" +
+                    //    "<Creators>" +
+                    //        "<Creator><Name>Jan Laursen</Name><RoleName>Radiovært</RoleName><RoleID>1</RoleID></Creator>" +
+                    //        "<Creator><Name>Benny Bidsk Jensen</Name><RoleName>Tekniker</RoleName><RoleID>2</RoleID></Creator>" +
+                    //        "<Creator><Name>Bjarke Ahlstrand</Name><RoleName>Radiovært</RoleName><RoleID>3</RoleID></Creator>" +
+                    //    "</Creators>" +
+                    //    "<Locations><Name>Svendborg</Name><Name>Svendborg havn</Name><Name>Svendborg banegård</Name></Locations>" +
+                    //    "<Identifiers><DR.ProductionNumber>666-666-6666</DR.ProductionNumber><DR.ArchiveNumber>2023</DR.ArchiveNumber><SB.DomsID>99</SB.DomsID></Identifiers></Larm.Program>";
+
 
                     self.m1 = handlexml(metadata[i]);
 
@@ -113,19 +112,20 @@
             html.mdtext(p, "!md_abstract", arkiv.Abstract);
             html.mdtext(p, "!md_description", arkiv.Description);
             html.mdtext(p, "!md_publisher", arkiv.Publisher);
-            html.mdtags(p, "!md_subjects", arkiv.Subjects.Subject);
+
+            html.mdtags(p, "!md_subjects", arkiv.Subjects);
 
             html.mdtable(p, "!md_contributors",
-                arkiv.Contributors.Contributor,
+                arkiv.Contributors,
                 ['Name', 'RoleName', 'RoleID'],
                 ['Name', 'RoleName', 'RoleID']);
 
             html.mdtable(p, "!md_creators",
-                arkiv.Creators.Creator,
+                arkiv.Creators,
                 ['Name', 'RoleName', 'RoleID'],
                 ['Name', 'RoleName', 'RoleID']);
 
-            html.mdtags(p, "!md_locations", arkiv.Locations[0].Name);
+            html.mdtags(p, "!md_locations", arkiv.Locations[0], "Name");
 
             html.mdgrid(p, "!md_identifiers", [
             ["DR produktionsnr:", arkiv.Identifiers.DR_ProductionNumber],
@@ -143,7 +143,7 @@
             html.mdtext(p, "Related Objects", larm.RelatedObjects);
 
             html.mdtable(p, "!md_contributors",
-                larm.Contributors.Contributor,
+                larm.Contributors,
                 ['Name', 'RoleName', 'Subject'],
                 ['Name', 'RoleName', 'Subject']);
 
